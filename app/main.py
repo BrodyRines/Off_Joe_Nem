169a08dc3be9ac492b8b696319a4c9b791e641f0
Brody-R <rinesbrody@gmail.com>
2026-03-03 13:36:29 -0500

    Added flask and HTML to display listings

--- app/main.py
+++ app/main.py
@@ -1,13 +1,26 @@
+from flask import Flask, render_template
 import configparser
 import requests
-
-config = configparser.ConfigParser()
-config.read('.env')
+import os
 
 
-key = config['MY_KEY']['KEY'] 
-url = 'https://csfloat.com/api/v1/listings/324288155723370196'
-headers = {'Authorization': key}
-resp = requests.get(url, headers=headers)
 
-print(resp.json())
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    config = configparser.ConfigParser()
+    config.read('.env')
+
+    key = config['MY_KEY']['KEY'] 
+    url = 'https://csfloat.com/api/v1/listings/324288155723370196'
+    headers = {'Authorization': key}
+    resp = requests.get(url, headers=headers)
+    data = resp.json() if resp.status_code == 200 else {'error': 'API failed'}
+    
+    return render_template('index.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+##print(resp.json())
